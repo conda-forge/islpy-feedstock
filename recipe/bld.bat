84ad31dedaf5e603f583755625a4374e055e591e
Isuru Fernando <isuruf@gmail.com>
2018-08-24 01:53:25 -0500

    Use isl 0.20

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,2 @@
-copy /y %RECIPE_DIR%\isl_declaration_macros_expanded.h .
 python configure.py --no-use-shipped-isl --no-use-gmp --isl-inc-dir=%LIBRARY_INC% --isl-lib-dir=%LIBRARY_LIB%
 python setup.py install --single-version-externally-managed --record record.txt
