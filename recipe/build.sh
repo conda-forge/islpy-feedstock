9419038d893addf25440b72f38a4e1043a31319c
Matthias Diener <mdiener@illinois.edu>
2021-07-05 21:06:20 -0500

    no shipped imath

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,2 +1,2 @@
-python configure.py --no-use-shipped-isl --no-use-gmp --isl-inc-dir=$PREFIX/include --isl-lib-dir=$PREFIX/include --use-barvinok
+python configure.py --no-use-shipped-isl --no-use-gmp --isl-inc-dir=$PREFIX/include --isl-lib-dir=$PREFIX/include --use-barvinok --no-use-shipped-imath
 python setup.py install --single-version-externally-managed --record record.txt
