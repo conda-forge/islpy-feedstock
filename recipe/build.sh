c6d21e53d2a4e920ac8d1840eba1dfa84e22f085
Matthias Diener <mdiener@illinois.edu>
2021-07-05 20:57:19 -0500

    add barvinok

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,2 +1,2 @@
-python configure.py --no-use-shipped-isl --no-use-gmp --isl-inc-dir=$PREFIX/include --isl-lib-dir=$PREFIX/include
+python configure.py --no-use-shipped-isl --no-use-gmp --isl-inc-dir=$PREFIX/include --isl-lib-dir=$PREFIX/include --use-barvinok
 python setup.py install --single-version-externally-managed --record record.txt
