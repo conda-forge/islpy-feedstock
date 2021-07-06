fcf1751b92b5c456a6dfb02bc72b2094da973917
Matthias Diener <mdiener@illinois.edu>
2021-07-05 21:26:40 -0500

    pip

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,9 @@
-pip install pcpp
+#!/bin/bash
+
+set -x
+
+$PYTHON -m pip install pcpp
 
 python configure.py --no-use-shipped-isl --no-use-gmp --isl-inc-dir=$PREFIX/include --isl-lib-dir=$PREFIX/include --use-barvinok --no-use-shipped-imath
+
 python setup.py install --single-version-externally-managed --record record.txt
