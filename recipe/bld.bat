97517425fc2c8894db9dbb348664109abd916de8
Andreas Kloeckner <inform@tiker.net>
2022-02-17 12:54:27 -0600

    Use PYTHON env var in build scripts

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,2 @@
-{{ PYTHON }} configure.py --no-use-shipped-isl --no-use-gmp --isl-inc-dir=%LIBRARY_INC% --isl-lib-dir=%LIBRARY_LIB%
-{{ PYTHON }} -m pip install . -vv
+%PYTHON% configure.py --no-use-shipped-isl --no-use-gmp --isl-inc-dir=%LIBRARY_INC% --isl-lib-dir=%LIBRARY_LIB%
+%PYTHON% -m pip install . -vv
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,2 +1,2 @@
-{{ PYTHON }} configure.py --no-use-shipped-isl --no-use-gmp --isl-inc-dir=$PREFIX/include --isl-lib-dir=$PREFIX/include
-{{ PYTHON }} -m pip install . -vv
+"${PYTHON}" configure.py --no-use-shipped-isl --no-use-gmp --isl-inc-dir="$PREFIX/include" --isl-lib-dir="$PREFIX/include"
+"${PYTHON}" -m pip install . -vv
