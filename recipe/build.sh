c56b068fca889f57aef38f7cad9f404c113e49b4
Isuru Fernando <idf2@illinois.edu>
2022-06-08 13:22:47 -0500

    Add a build variant with barvinok

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,2 +1,12 @@
-"${PYTHON}" configure.py --no-use-shipped-isl --no-use-gmp --isl-inc-dir="$PREFIX/include" --isl-lib-dir="$PREFIX/include"
+if [[ "${with_barvinok}" == "1" ]]; then
+  EXTRA_CONFIGURE_OPTIONS="--use-barvinok"
+fi
+
+"${PYTHON}" configure.py \
+  --no-use-shipped-isl \
+  --no-use-gmp \
+  --isl-inc-dir="$PREFIX/include" \
+  --isl-lib-dir="$PREFIX/include" \
+  $EXTRA_CONFIGURE_OPTIONS
+
 "${PYTHON}" -m pip install . -vv
