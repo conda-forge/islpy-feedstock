b15c5b45d23823a00329acb9d719ad94aa71f71c
Matthias Diener <matthias.diener@gmail.com>
2023-09-18 19:52:51 -0500

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,5 @@
 echo "#include <string.h>" >> src\wrapper\wrap_isl.hpp
+type src\wrapper\wrap_isl.hpp
 
 %PYTHON% configure.py --no-use-shipped-isl --no-use-gmp --isl-inc-dir=%LIBRARY_INC% --isl-lib-dir=%LIBRARY_LIB%
 %PYTHON% -m pip install . -vv
