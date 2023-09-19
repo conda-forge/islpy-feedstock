462b8e5f041bd7d237ed7e5da26240a5ebfe7afa
Matthias Diener <mdiener@illinois.edu>
2023-09-18 19:12:00 -0500

    add include for win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,4 @@
+echo "#include <string.h>" >> src\wrapper\wrap_isl.hpp
+
 %PYTHON% configure.py --no-use-shipped-isl --no-use-gmp --isl-inc-dir=%LIBRARY_INC% --isl-lib-dir=%LIBRARY_LIB%
 %PYTHON% -m pip install . -vv
