89f557a0efe813b4705539c47c150c7170f7bfba
Matthias Diener <mdiener@illinois.edu>
2025-02-17 21:42:30 -0600

    try cgthreads

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,4 +4,7 @@ sed -i "/LTO/d" CMakeLists.txt
 
 %PYTHON% configure.py --no-use-shipped-isl --no-use-gmp --isl-inc-dir=%LIBRARY_INC% --isl-lib-dir=%LIBRARY_LIB%
 
+
+sed -i "$ a add_compile_options('/cgthreads1')" CMakeLists.txt
+
 %PYTHON% -m pip install . -vv
