c72d2da5f9a127f4f5f1d7b29ba34c021ab1c93e
Matthias Diener <mdiener@illinois.edu>
2025-02-17 23:15:55 -0600

    t

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,7 +5,7 @@ sed -i "/LTO/d" CMakeLists.txt
 %PYTHON% configure.py --no-use-shipped-isl --no-use-gmp --isl-inc-dir=%LIBRARY_INC% --isl-lib-dir=%LIBRARY_LIB%
 
 
-sed -i "$ a add_compile_options(/cgthreads1)" CMakeLists.txt
+sed -i "$ a target_compile_options(_isl PRIVATE /cgthreads1)" CMakeLists.txt
 
 type CMakeLists.txt
 
