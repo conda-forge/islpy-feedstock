bf26bd1ecf9433dc3662c128d53ce7970ecffa1d
Andreas Klöckner <inform@tiker.net>
2025-06-11 10:56:46 -0500

    Fix no-stub-build CMake args

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,7 +12,7 @@ fi
 # stub generation needs to import the built binary in order to work. Instead, we're
 # grabbing the stubs from a binary wheel on the package index here.
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
-  EXTRA_PIP_INSTALL_FLAGS="--config-settings=cmake.define=GENERATE_STUBS=OFF $EXTRA_PIP_INSTALL_FLAGS"
+  EXTRA_PIP_INSTALL_FLAGS="-Ccmake.args=-DGENERATE_STUBS=OFF $EXTRA_PIP_INSTALL_FLAGS"
   unset PIP_NO_INDEX
   pip download "islpy==$PKG_VERSION" --only-binary :all: --platform macosx_11_0_x86_64
   export PIP_NO_INDEX=True
