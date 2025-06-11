66267e9ecb2d197baf30c3ed1236e06002999df7
Andreas Klöckner <inform@tiker.net>
2025-06-11 08:53:37 -0500

    Grab stubs from binary wheel if cross-compiling

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 set -ex
+EXTRA_PIP_INSTALL_FLAGS=""
 
 if [[ "${with_barvinok}" == "True" ]]; then
-  EXTRA_PIP_INSTALL_FLAGS="--config-settings=cmake.define.USE_BARVINOK=ON"
+  EXTRA_PIP_INSTALL_FLAGS="--config-settings=cmake.define.USE_BARVINOK=ON $EXTRA_PIP_INSTALL_FLAGS"
+fi
+
+# Generating the stubs seems near-impossible in a cross-copmilation situation, since
+# stub generation needs to import the built binary in order to work. Instead, we're
+# grabbing the stubs from a binary wheel on the package index here.
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
+  EXTRA_PIP_INSTALL_FLAGS="--config-settings=cmake.define=GENERATE_STUBS=OFF $EXTRA_PIP_INSTALL_FLAGS"
+  pip download "islpy==$PKG_VERSION" --only-binary :all:
+  unzip *.whl islpy/_isl.pyi 
 fi
 
 # Finagle libxcrypt headers into build
