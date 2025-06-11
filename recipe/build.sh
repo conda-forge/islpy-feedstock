5fb28997267c45229e35d4a10b91fd7be197c5bf
Andreas Klöckner <inform@tiker.net>
2025-06-11 10:29:51 -0500

    Log PKG_VERSION

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -ex
+set -o pipefail
 EXTRA_PIP_INSTALL_FLAGS=""
 
 if [[ "${with_barvinok}" == "True" ]]; then
@@ -12,6 +13,7 @@ fi
 # grabbing the stubs from a binary wheel on the package index here.
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
   EXTRA_PIP_INSTALL_FLAGS="--config-settings=cmake.define=GENERATE_STUBS=OFF $EXTRA_PIP_INSTALL_FLAGS"
+  echo "Finding wheel URL for $PKG_VERSION..."
   WHEEL_URL="$(curl https://pypi.org/pypi/islpy/json | jq -c '.releases."$PKG_VERSION" | .[] | select(.packagetype == "bdist_wheel")'  | jq -r --slurp '.[0].url')"
   echo "Downloading wheel from $WHEEL_URL..."
   curl -O "$WHEEL_URL"
